223388fcdfab085002299e7e6fd0af6704fad858
Segev Raz <welegionsr@gmail.com>
2019-07-14 08:31:02 +0300

    fix for login router endpoint

--- routes/login.js
+++ routes/login.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 var passport = require('../auth');
 
-router.post('/login', passport.authenticate('local'), (req, res) => {
+router.post('/', passport.authenticate('local'), (req, res) => {
     console.log(req.body);
     if(req.user) {
         res.status(200).send(req.user);
